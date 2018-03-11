23711057175553d61806ce3af36428b9d22982e1
soba1104 <shiba@rvm.jp>
2018-03-12 02:35:54 +0900

    ファイルを読み進める処理を実装

--- mpfp/src/main.rs
+++ mpfp/src/main.rs
@@ -2,14 +2,41 @@ use std::env;
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::Error;
+use std::io::Cursor;
+use std::io::SeekFrom;
+use std::io::ErrorKind;
+
+extern crate byteorder;
+use byteorder::{BigEndian, ReadBytesExt};
 
 fn process(path: &String) -> Result<(), Error> {
     println!("process: {:?}.", path);
-    let mut buf: [u8; 4] = [0; 4];
+    let mut buf: [u8; 8] = [0; 8];
     let mut file = File::open(path)?;
-    file.read_exact(&mut buf)?;
+    let mut position: u64 = 0;
+
+    loop {
+        match file.read_exact(&mut buf) {
+            Ok(()) => {
+                let mut c = Cursor::new(buf);
+                let size = c.read_u32::<BigEndian>()?;
+                let mut name = String::new();
+                c.read_to_string(&mut name)?;
+                println!("read: buf = {:?}, size = {:?}, name = {:?}.", buf, size, name);
+                position += size as u64;
+                file.seek(SeekFrom::Start(position))?;
+            },
+            Err(ref err) if err.kind() == ErrorKind::UnexpectedEof => {
+                println!("done.");
+                break;
+            }
+            Err(err) => {
+                println!("unexpected error: {:?}.", err);
+                break;
+            }
+        }
+    }
     // TODO 指定したサイズよりも読み込んだ量が少なかったらエラーを返す。
-    println!("head: {:?}.", buf);
     Ok(())
 }
 
